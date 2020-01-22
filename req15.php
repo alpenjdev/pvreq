720fe6ca68c8196eb54c4e0227d87ed0af3afd35
alpenjdev <59833895+alpenjdev@users.noreply.github.com>
2020-01-22 16:41:21 +0100

    Update req15.php

--- req15.php
+++ req15.php
@@ -1,2 +1,2 @@
 
-{ "app": [ { "url": "https://co.aptoide.com", "image": "https://static.apkpure.com/www/static/imgs/logo_new.png", "update": "yes" } ] }
+{ "app": [ { "url": "https://co.aptoide.com", "image": "https://static.apkpure.com/www/static/imgs/logo_new.png", "update": "no" } ] }
